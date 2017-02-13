67f201c8d4684a04f186b37a320218c8457e2053
nakaearth <naka5313@gmail.com>
2017-02-13 13:35:07 +0900

    neural networkのサンプル

---
+++ array_calculate_sample.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+a = np.array([[1, 2], [3, 4]])
+b = np.array([[5, 6], [7, 8]])
+print(a.shape)
+print(np.dot(a, b))
+
+aa = np.array([[1,2,3], [4, 5, 6]])
+bb = np.array([[1, 2], [3, 4], [5, 6]])
+
+print(np.dot(aa, bb))
+
